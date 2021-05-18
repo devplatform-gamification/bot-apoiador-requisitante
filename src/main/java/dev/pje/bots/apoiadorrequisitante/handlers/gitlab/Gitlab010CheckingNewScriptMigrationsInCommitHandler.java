4260cea14435e327f8060938601e5a3291af61bd
Zeniel Chaves <zeniel@cnj.jus.br>
2021-05-18 17:34:12 -0300

    Correcao nome commit reordenacao scripts

--- src/main/java/dev/pje/bots/apoiadorrequisitante/handlers/gitlab/Gitlab010CheckingNewScriptMigrationsInCommitHandler.java
+++ src/main/java/dev/pje/bots/apoiadorrequisitante/handlers/gitlab/Gitlab010CheckingNewScriptMigrationsInCommitHandler.java
@@ -160,7 +160,7 @@ public class Gitlab010CheckingNewScriptMigrationsInCommitHandler {
 			}
 			// encaminha para alteracao no gitlab
 			String identificadorCommit = StringUtils.isNotBlank(issueKey) ? issueKey : "[RELEASE]";
-			String commitMessage = identificadorCommit + " Reordenando scripts do commit (" + lastCommitId + ")";
+			String commitMessage = "[" + identificadorCommit + "] Reordenando scripts do commit (" + lastCommitId + ")";
 			gitlabService.moveFiles(project, branchName, lastCommitId, scriptsToChange, commitMessage);
 		}
 	}
